b0cd732ac7c2c63f937552bf2ad60fc890e6eca4
Thomas Spiesser <thomas.spiesser@gmail.com>
2014-12-07 14:23:07 +0100

    adds chat and semantic-ui for styling, styling unfinished

--- chatExample.js
+++ chatExample.js
@@ -1,44 +1,81 @@
 Messages = new Meteor.Collection("messages");
 
-var message = {
-      message: text,
-      user: user,
-      userName: userName,
-      timestamp: new Date()
-    };
-
-var NonEmptyString = Match.Where(function (x) {
-  check(x, String);
-  return x.length !== 0;
-});
-
-// Meteor.methods({
-//   insertMessage: function (options) {
-//     check(options.pinboardId, NonEmptyString);
-//     check(options.message, {
-//       message: NonEmptyString,
-//       user: NonEmptyString,
-//       userName: NonEmptyString,
-//       timestamp: Date
-//     });
-//     Messages.update(options.messageId, {$push: {messages: options.message}});
-//   },
-// })
-
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.isActive("chatId", "active");
-
-  Template.messages.helpers({
-    messages: function () {
-      return Messages.find( {user: Meteor.userId()}, { sort: { timestamp: -1 } })
+  Template.body.helpers({
+    error: function() {
+    return Session.get("createError");
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
+  Template.login.helpers({
+    randomUsername: function () {
+      var user = Random.choice(["scary", "sweet", "tricky", "green", "holy", "red", "funny", "sophisticated", "smart"])
+      var name = Random.choice(["elephant", "mouse", "rabbit", "cat", "dog", "weasel", "horse", "tiger"])
+      var username = user + ' ' + name
+      return username;
+    },
+    randomPassword: function () {
+      var password = Math.random().toString(36).substr(2,6);
+      return password;
+    }
+  });
+
+  Template.login.events({
+    'click #signUpButton': function (event,template) {
+      var username = template.find('#username').value;
+      var password = template.find('#password').value;
+
+      Accounts.createUser({username: username, password : password}, function(err){
+        if (err) {
+          Session.set( "createError", "Sorry, "+err.reason );
+        } else {
+          Session.set( "createError", '' );
+        }
+      });
+      return false;
+    }
+  });
+
+  Template.messages.helpers({
+    messages: function () {
+      return Messages.find( {}, { sort: { timestamp: -1 } })
+    }
+  });
+
+  Template.messages.events({
+    'click .sendNewMessage': function (event, template) {
+      var text = template.find('#inputField').value;
+      var user = Meteor.userId();
+      var username = Meteor.user().username;
+
+      if (text.length) {
+        var message = {
+          message: text,
+          user: user,
+          username: username,
+          timestamp: new Date()
+        };
+        Messages.insert(message, function (error, result) {
+          if (error) {
+            Session.set( "createError", "Sorry, "+err.reason );
+          }
+          else {
+            $('#inputField').val("");
+            Session.set( "createError", '' );
+          }
+        });
+      } else {
+        Session.set( "createError", "Please, write something" );
+      }
+    return false;
+    }
+  });
+
+  Template.logout.events({
+    'click #logoutButton': function () {
+      var id = Meteor.userId();
+      Meteor.logout;
+      Meteor.call('Remove', id);
     }
   });
 }
@@ -47,4 +84,9 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+  Meteor.methods({
+    Remove: function (id) {
+      Meteor.users.remove({_id: id});
+    }
+  })
 }
